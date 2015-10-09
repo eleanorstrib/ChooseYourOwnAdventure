57282173ef469ed1ccb154ee91e81977f3ff3351
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-08 17:51:54 -0700

    created nav controller and first view controller for story, need to play with constraints to get working in portrait, landscape

---
+++ ChooseYourOwnAdventure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChooseYourOwnAdventure
+//
+//  Created by Eleanor Stribling on 10/8/15.
+//  Copyright (c) 2015 eleanorstrib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
